b60589eb0467fa60bbcf03448eaa884f993dc07b
Adrc95 <awinf204@gmail.com>
2026-08-27 10:59:37 +0200

    Initial Commit

---
+++ rickyandmorty/rickyandmortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickyandmortyApp.swift
+//  rickyandmorty
+//
+//  Created by Adrian on 27/8/26.
+//
+
+import SwiftUI
+
+@main
+struct rickyandmortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
